f0799b2b603ccf3152b306485298bbb107a5d3e8
Wágner Árpád <wgnrar@gmail.com>
2022-04-11 03:13:41 +0200

    Transformed User class to ORM

--- app/src/main/java/aut/bme/hu/boltie/model/User.kt
+++ app/src/main/java/aut/bme/hu/boltie/model/User.kt
@@ -1,15 +1,30 @@
 package aut.bme.hu.boltie.model
 
+import androidx.room.*
+
 enum class UserType {
     Restocker, // TODO: Actual english name
     Seller,
     Manager
 }
 
+@Entity(tableName = "user")
 class User(
-    val type: UserType,
-    val name: String,
-    val id: String
-) {
+    @PrimaryKey(autoGenerate = true) var localId: Long,
+    @ColumnInfo(name = "userID") val userId: String,
+    @ColumnInfo(name = "userType") val type: UserType,
+    @ColumnInfo(name = "name") val name: String
+) { }
 
+@Dao
+interface UserDAO {
+    // TODO: This method
+    @Query("""SELECT * FROM user""")
+    fun getCurrentUser(): User
+
+    @Insert
+    fun insertUsers(vararg users: User)
+
+    @Delete
+    fun deleteUser(user: User)
 }
